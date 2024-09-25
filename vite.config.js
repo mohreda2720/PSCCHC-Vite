cb9bed20143cf7fdb0b5bb352c059b28a99aac0f
Mohamed Reda Radwan <84078938+mohreda2720@users.noreply.github.com>
2024-09-25 14:22:18 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://pscchcit-001-site1.atempurl.com', // Your API URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+    port: 3000
   }
 });
